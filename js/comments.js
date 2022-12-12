c792e27669333b7d1183be93d8380dfa942faf31
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-12-12 10:10:58 +0500

    feat: add commits js file

---
+++ js/comments.js
@@ -0,0 +1,48 @@
+// let swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   loop: true,
+//   navigation: {
+//     nextEl: ".slideNextBtn",
+//     prevEl: ".slidePrevBtn",
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//     },
+//     375: {
+//       slidesPerView: 1,
+//     },
+//     425: {
+//       slidesPerView: 2,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//     },
+
+//   },
+// });
+
+// const openMenu = document.querySelector("#burgerNav");
+// const closeMenu = document.querySelector("#closeNav");
+// const mobileMenu = document.querySelector("#mobileNav");
+
+// openMenu.onclick = () => {
+//   mobileMenu.classList.remove("hidden");
+//   openMenu.classList.add("hidden");
+//   closeMenu.classList.remove("hidden");
+// };
+
+// closeMenu.onclick = () => {
+//   mobileMenu.classList.add("hidden");
+//   openMenu.classList.remove("hidden");
+//   closeMenu.classList.add("hidden");
+// };
+
+// function clickImg(smallImg){
+//   let fullImg = document.getElementById("imageBox");
+//   fullImg.src = smallImg.src
+// }
